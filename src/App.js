04eeec0e3569086427584108d635c6e9fcad62ef
Daria-Valitskaya <75114227+Daria-Valitskaya@users.noreply.github.com>
2021-09-04 00:51:34 +0300

    api готов

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Component } from "react";
+import ApiServise from "./Components/API";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    images: [],
+    loading: false,
+  };
+  componentDidMount() {
+    this.setState({ loading: true });
+    ApiServise("cat", 1)
+      .then((images) => this.setState({ images }))
+      .finally(() => this.setState({ loading: false }));
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.loading && <div>Zagruzhaem</div>}
+        {this.state.images && <div> krasivoe </div>}
+      </div>
+    );
+  }
 }
-
-export default App;
